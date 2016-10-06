ff837848716973f52b362d08cfcd09f18700d086
HereAround <M.Bies@thphys.uni-heidelberg.de>
2016-10-06 10:37:21 +0200

    Line-breaks in ICT-curves... looks nicer

--- gap/ICTCurves.gd
+++ gap/ICTCurves.gd
@@ -32,10 +32,10 @@ DeclareCategory( "IsICTCurve",
 ##############################################################################################
 
 #! @Description
-#! The arguments are a smooth and complete toric variety $X_\Sigma$ and two non-negative and distinct integers $i,j$. We then consider
-#! the i-th and j-th maximal cones $\sigma_i$ and $\sigma_j$. ! If $\tau := \sigma_i \cap \sigma_j$ satisfies 
-#! $\text{dim} \left( \tau \right) = \text{dim} \left( \sigma_1 \right) - 1$, then $\tau$ corresponds to an ICT-curve. 
-#! We then construct this very ICT-curve.
+#! The arguments are a smooth and complete toric variety $X_\Sigma$ and two non-negative and distinct integers $i,j$.
+#! We then consider the i-th and j-th maximal cones $\sigma_i$ and $\sigma_j$. ! If $\tau := \sigma_i \cap \sigma_j$ 
+#! satisfies $\text{dim} \left( \tau \right) = \text{dim} \left( \sigma_1 \right) - 1$, then $\tau$ corresponds to 
+#! an ICT-curve. We then construct this very ICT-curve.
 #! @Returns an ICT curve
 #! @Arguments X_Sigma, i, j
 DeclareOperation( "ICTCurve",
@@ -75,29 +75,33 @@ DeclareAttribute( "RayGenerators",
                    IsICTCurve );
 
 #! @Description
-#! The argument is an ICT curve $C$. The output is the list of variables whose simultaneous vanishing locus cuts out this curve.
+#! The argument is an ICT curve $C$. The output is the list of variables whose simultaneous vanishing locus 
+#! cuts out this curve.
 #! @Returns a list
 #! @Arguments C
 DeclareAttribute( "DefiningVariables",
                    IsICTCurve );
 
 #! @Description
-#! The argument is an ICT curve $C$. The output is the f.p. graded S-module which sheafifes to the structure sheaf of this curve $C$.
+#! The argument is an ICT curve $C$. The output is the f.p. graded S-module which sheafifes to the structure sheaf 
+#! of this curve $C$.
 #! @Returns a f.p. graded module
 #! @Arguments C
 DeclareAttribute( "StructureSheaf",
                    IsICTCurve );
 
 #! @Description
-#! The argument is an ICT curve $C$. The output is the integral vector $u$ used to compute intersection products with Cartier divisors.
+#! The argument is an ICT curve $C$. The output is the integral vector $u$ used to compute intersection products 
+#! with Cartier divisors.
 #! @Returns a list of integers
 #! @Arguments C
 DeclareAttribute( "IntersectionU",
                    IsICTCurve );
 
 #! @Description
-#! The argument is an ICT curve $C$. The output is a list with the intersection numbers of a canonical base of the class group. This basis
-#! is to take $\left( e_1, \dots, e_k \right)$ with $e_i = \left( 0, \dots, 0, 1, 0, \dots, 0 \right) \in \text{Cl} \left( X_\Sigma \right)$.
+#! The argument is an ICT curve $C$. The output is a list with the intersection numbers of a canonical base 
+#! of the class group. This basis is to take $\left( e_1, \dots, e_k \right)$ with $e_i = \left( 0, \dots, 0, 
+#! 1, 0, \dots, 0 \right) \in \text{Cl} \left( X_\Sigma \right)$.
 #! @Returns a list of integers
 #! @Arguments C
 DeclareAttribute( "IntersectionList",
@@ -112,16 +116,16 @@ DeclareAttribute( "IntersectionList",
 ##############################################################################################
 
 #! @Description
-#! For a smooth and complete toric variety $X_\Sigma$, this method computes a list of all ICT-curves in $X_\Sigma$. Note that those
-#! curves can be numerically equivalent.
+#! For a smooth and complete toric variety $X_\Sigma$, this method computes a list of all ICT-curves in $X_\Sigma$. 
+#! Note that those curves can be numerically equivalent.
 #! @Returns a list of ICT-curves.
 #! @Arguments X_Sigma
 DeclareAttribute( "ICTCurves",
                   IsToricVariety );
 
 #! @Description
-#! Given an ICT-curve $C$ and a divisor $D$ in a smooth and complete toric variety $X_\Sigma$, this method computes their intersection
-#! product.
+#! Given an ICT-curve $C$ and a divisor $D$ in a smooth and complete toric variety $X_\Sigma$, this method computes
+#! their intersection product.
 #! @Returns an integer
 #! @Arguments C,D
 DeclareOperation( "IntersectionProduct",
@@ -159,18 +163,18 @@ DeclareCategory( "IsProper1Cycle",
 ##############################################################################################
 
 #! @Description
-#! For a smooth and complete toric variety $X_\Sigma$, this method computes a list of all ICT-curves which are not numerically equivalent.
-#! We use this list of ICT-curves as a basis of proper 1-cycles on $X_\Sigma$ in the constructor below, when computing the intersection form
-#! and the Nef-cone.
+#! For a smooth and complete toric variety $X_\Sigma$, this method computes a list of all ICT-curves which are not 
+#! numerically equivalent. We use this list of ICT-curves as a basis of proper 1-cycles on $X_\Sigma$ in the 
+#! constructor below, when computing the intersection form and the Nef-cone.
 #! @Returns a list of ICT-curves.
 #! @Arguments X_Sigma
 DeclareAttribute( "GeneratorsOfProper1Cycles",
                   IsToricVariety );
 
 #! @Description
-#! The arguments are a smooth and complete toric variety $X_\Sigma$ and a list of integers. We then use the integers in this list
-#! as 'coordinates' of the proper 1-cycle with respect to the list of proper 1-cycles produced by the previous method.
-#! We then return the corresponding proper 1-cycle.
+#! The arguments are a smooth and complete toric variety $X_\Sigma$ and a list of integers. We then use the integers
+#! in this list as 'coordinates' of the proper 1-cycle with respect to the list of proper 1-cycles produced by the 
+#! previous method. We then return the corresponding proper 1-cycle.
 #! @Returns a proper 1-cycle
 #! @Arguments X_Sigma, list
 DeclareOperation( "Proper1Cycle",
@@ -194,8 +198,8 @@ DeclareAttribute( "AmbientToricVariety",
                    IsICTCurve );
 
 #! @Description
-#! The argument is a proper 1-cycle. We then return the underlying group element (with respect to the generators computed from the method
-#! \emph{GeneratorsOfProper1Cycles}).
+#! The argument is a proper 1-cycle. We then return the underlying group element (with respect to the generators 
+#! computed from the method \emph{GeneratorsOfProper1Cycles}).
 #! @Returns a list
 #! @Arguments C
 DeclareAttribute( "UnderlyingGroupElement",
@@ -210,8 +214,8 @@ DeclareAttribute( "UnderlyingGroupElement",
 ##############################################################################################
 
 #! @Description
-#! Given a proper 1-cycle $C$ and a divisor $D$ in a smooth and complete toric variety $X_\Sigma$, this method computes their intersection
-#! product.
+#! Given a proper 1-cycle $C$ and a divisor $D$ in a smooth and complete toric variety $X_\Sigma$, this method
+#! computes their intersection product.
 #! @Returns an integer
 #! @Arguments C,D
 DeclareOperation( "IntersectionProduct",
@@ -220,8 +224,8 @@ DeclareOperation( "IntersectionProduct",
                  [ IsToricDivisor, IsProper1Cycle ] );
 
 #! @Description
-#! Given a simplicial and complete toric variety, we can use proposition 6.4.1 of Cox-Schenk-Litte to compute 
-#! the intersection form <M>N^1 \left( X_\Sigma \right) \times N_1 \left( X_\Sigma \right) \to \mathbb{R} </M>. 
+#! Given a simplicial and complete toric variety, we can use proposition 6.4.1 of Cox-Schenk-Litte to compute
+#! the intersection form <M>N^1 \left( X_\Sigma \right) \times N_1 \left( X_\Sigma \right) \to \mathbb{R} </M>.
 #! We return a list of lists that encodes this mapping.
 #! @Returns a list of lists
 #! @Arguments vari
