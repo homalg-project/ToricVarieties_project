30583349822df221833612949d93b9cd104bce9f
HereAround <M.Bies@thphys.uni-heidelberg.de>
2017-03-31 13:21:07 +0200

    Worked over toric varieties and sheaf cohomology for toric varieties

--- gap/DegreeXLayer.gd
+++ gap/DegreeXLayer.gd
@@ -9,6 +9,53 @@
 #########################################################################################
 
 
+
+##########################################################
+##
+#! @Section DegreeXLayers of the Cox ring
+##
+##########################################################
+
+#! @Description
+#! Given a smooth and complete toric variety and a list of integers (= degree) corresponding to an element of 
+#! the class group of the variety, this method return a list of integer valued lists. These lists correspond 
+#! to the exponents of the monomials of degree in the Cox ring of this toric variety.
+#! @Returns a list of lists of integers
+#! @Arguments vari, degree
+DeclareOperation( "Exponents",
+               [ IsToricVariety, IsList ] );
+
+#! @Description
+#! Given a smooth and complete toric variety and a list of integers (= degree) corresponding to an element
+#! of the class group of the variety, this method returns the list of all monomials in the Cox ring of the
+#! given degree. This method uses NormalizInterface. 
+#! @Returns a list
+#! @Arguments vari, degree
+DeclareOperation( "MonomsOfCoxRingOfDegreeByNormaliz",
+               [ IsToricVariety, IsList ] );
+
+#! @Description
+#! Given a smooth and complete toric variety and a list of integers (= degree) corresponding to an element
+#! of the class group of the variety, this method returns the list of all monomials in the Cox ring of the
+#! given degree. This method uses NormalizInterface. 
+#! @Returns a list
+#! @Arguments vari, degree
+DeclareOperation( "DegreeXLayer",
+                 [ IsToricVariety, IsList ] );
+
+#! @Description
+#! Given a smooth and complete toric variety, a list of integers (= degree) corresponding to an element
+#! of the class group of the variety and two non-negative integers i and l, this method returns a list
+#! of column matrices. The columns are of length l and have at position i the monoms of the Coxring of degree 'degree'.
+#! @Returns a list of matrices
+#! @Arguments vari, degree, i, l
+DeclareOperation( "DegreeXLayerVectorsAsColumnMatrices",
+                 [ IsToricVariety, IsList, IsPosInt, IsPosInt ] );
+
+
+
+
+
 ##############################################################################################
 ##
 #! @Section GAP category of DegreeXLayerVectorSpaces(Morphisms)
