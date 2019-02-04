ace223bd53cae70dcc2217f13192a960ca78ca2e
Martin Bies <MBies87@googlemail.com>
2019-02-04 16:09:19 +0100

    Declared convenience methods

--- gap/Functions.gd
+++ gap/Functions.gd
@@ -27,10 +27,12 @@
 DeclareOperation( "points2chiro",
                   [ IsList, IsList, IsList ] );
 
-#!
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments List1
 DeclareOperation( "points2chiro",
                   [ IsList ] );
-
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -43,6 +45,13 @@ DeclareOperation( "chiro2dual",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2dual",
+                  [ IsString ] );
+
+#! @Description
 #! The first argument is a string encoding the chiro and the
 #! second a list encoding an (optional) sample triangulation. 
 #! The third argument is a list of strings, consisting of the
@@ -53,6 +62,13 @@ DeclareOperation( "chiro2circuits",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2circuits",
+                  [ IsString ] );
+
+#! @Description
 #! The first argument is a string encoding the chiro and the
 #! second a list encoding an (optional) sample triangulation. 
 #! The third argument is a list of strings, consisting of the
@@ -61,7 +77,14 @@ DeclareOperation( "chiro2circuits",
 #! @Arguments String, List2, List3
 DeclareOperation( "chiro2cocircuits",
                   [ IsString, IsList, IsList ] );
-                  
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2cocircuits",
+                  [ IsString ] );
+
 #! @Description
 #! The first argument is a string encoding the chiro and the
 #! second a list encoding an (optional) sample triangulation. 
@@ -73,12 +96,26 @@ DeclareOperation( "cocircuits2facets",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "cocircuits2facets",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns String
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2facets",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments List1
+DeclareOperation( "points2facets",
+                  [ IsList ] );
 
 #! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
@@ -89,12 +126,26 @@ DeclareOperation( "points2nflips",
                   [ IsList, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns Integer
+#! @Arguments List1
+DeclareOperation( "points2nflips",
+                  [ IsList ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns String
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2flips",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments List1
+DeclareOperation( "points2flips",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -107,12 +158,26 @@ DeclareOperation( "chiro2placingtriang",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2placingtriang",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2placingtriang",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2placingtriang",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -125,12 +190,26 @@ DeclareOperation( "chiro2finetriang",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2finetriang",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2finetriang",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2finetriang",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -143,12 +222,26 @@ DeclareOperation( "chiro2triangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2triangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns String
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2triangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments List1
+DeclareOperation( "points2triangs",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -161,12 +254,26 @@ DeclareOperation( "chiro2ntriangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2ntriangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2ntriangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2ntriangs",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -179,12 +286,26 @@ DeclareOperation( "chiro2finetriangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2finetriangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2finetriangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2finetriangs",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -197,12 +318,26 @@ DeclareOperation( "chiro2nfinetriangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2nfinetriangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2nfinetriangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2nfinetriangs",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -215,12 +350,26 @@ DeclareOperation( "chiro2alltriangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2alltriangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2alltriangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2alltriangs",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -233,12 +382,26 @@ DeclareOperation( "chiro2nalltriangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2nalltriangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2nalltriangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2nalltriangs",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -251,12 +414,26 @@ DeclareOperation( "chiro2allfinetriangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2allfinetriangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2allfinetriangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2allfinetriangs",
+                  [ IsList ] );
 
 #! @Description
 #! The first argument is a string encoding the chiro and the
@@ -269,9 +446,23 @@ DeclareOperation( "chiro2nallfinetriangs",
                   [ IsString, IsList, IsList ] );
 
 #! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns String
+#! @Arguments String
+DeclareOperation( "chiro2nallfinetriangs",
+                  [ IsString ] );
+
+#! @Description
 #! The first two lists are the input required by topcom. The third is a list of strings, consisting
 #! of the options supported by topcom.
 #! @Returns List
 #! @Arguments List1, List2, List3
 DeclareOperation( "points2nallfinetriangs",
                   [ IsList, IsList, IsList ] );
+
+#! @Description
+#! Convenience method of the above with List2 = [], List3 = []
+#! @Returns List
+#! @Arguments List1
+DeclareOperation( "points2nallfinetriangs",
+                  [ IsList ] );
