ebefc5c97e2832d7b5d87c01ed40a0eca91faf3f
Martin Bies <MBies87@googlemail.com>
2019-07-26 14:28:47 +0200

    Move methods that compute all monomials of given degree in Cox ring to ToricVarietiesAdditionalProperties

--- gap/ToricVarietiesAdditionalPropertiesForCAP.gd
+++ gap/ToricVarietiesAdditionalPropertiesForCAP.gd
@@ -9,11 +9,11 @@
 ################################################################################################
 
 
-######################
+###################################################
 ##
-#! @Section Properties
+#! @Section Input check for cohomology computations
 ##
-######################
+###################################################
 
 #! @Description
 #! Returns if the given variety V is a valid input for cohomology computations.
@@ -27,11 +27,11 @@ DeclareProperty( "IsValidInputForCohomologyComputations",
                   IsToricVariety );
 
 
-######################
+####################################################################
 ##
-#! @Section Attributes
+#! @Section Stanley-Reisner and irrelevant ideal via FpGradedModules
 ##
-######################
+####################################################################
 
 #! @Description
 #! Returns the irrelevant left ideal of the Cox ring of the variety <A>vari</A>, using the language of CAP.
@@ -78,3 +78,46 @@ DeclareAttribute( "SfpgrmodLeft",
 #! @Arguments variety
 DeclareAttribute( "SfpgrmodRight",
                  IsToricVariety );
+
+
+##########################################################
+##
+#! @Section Monoms of given degree in the Cox ring
+##
+##########################################################
+
+#! @Description
+#! Given a smooth and complete toric variety and a list of integers (= degree) corresponding to an element of 
+#! the class group of the variety, this method return a list of integer valued lists. These lists correspond 
+#! to the exponents of the monomials of degree in the Cox ring of this toric variety.
+#! @Returns a list of lists of integers
+#! @Arguments vari, degree
+DeclareOperation( "Exponents",
+               [ IsToricVariety, IsList ] );
+
+#! @Description
+#! Given a smooth and complete toric variety and a list of integers (= degree) corresponding to an element
+#! of the class group of the variety, this method returns the list of all monomials in the Cox ring of the
+#! given degree. This method uses NormalizInterface. 
+#! @Returns a list
+#! @Arguments vari, degree
+DeclareOperation( "MonomsOfCoxRingOfDegreeByNormaliz",
+               [ IsToricVariety, IsList ] );
+
+#! @Description
+#! Given a smooth and complete toric variety and a list of integers (= degree) corresponding to an element
+#! of the class group of the variety, this method returns the list of all monomials in the Cox ring of the
+#! given degree. This method uses NormalizInterface. 
+#! @Returns a list
+#! @Arguments vari, degree
+DeclareOperation( "DegreeXLayer",
+                 [ IsToricVariety, IsList ] );
+
+#! @Description
+#! Given a smooth and complete toric variety, a list of integers (= degree) corresponding to an element
+#! of the class group of the variety and two non-negative integers i and l, this method returns a list
+#! of column matrices. The columns are of length l and have at position i the monoms of the Coxring of degree 'degree'.
+#! @Returns a list of matrices
+#! @Arguments vari, degree, i, l
+DeclareOperation( "DegreeXLayerVectorsAsColumnMatrices",
+                 [ IsToricVariety, IsList, IsPosInt, IsPosInt ] );
