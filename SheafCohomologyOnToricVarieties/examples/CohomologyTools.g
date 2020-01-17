f51b7f2dce7d06c7610a042350e0f2fbfd86a073
Martin Bies <MBies87@googlemail.com>
2020-01-17 16:42:32 +0000

    Interface to old graded modules has been moved to ToolsForFPGradedModules -> Delete these functions in SheafCohomologyOnToricVarieties

--- SheafCohomologyOnToricVarieties/examples/CohomologyTools.g
+++ SheafCohomologyOnToricVarieties/examples/CohomologyTools.g
@@ -2,32 +2,6 @@
 
 #! @Section Examples
 
-#! @Subsection Conversion of modules
-
-LoadPackage( "SheafCohomologyOnToricVarieties" );
-
-#! @Example
-P1 := ProjectiveSpace( 1 );
-#! <A projective toric variety of dimension 1>
-P1xP1 := P1 * P1;
-#! <A projective toric variety of dimension 2
-#! which is a product of 2 toric varieties>
-irP1xP1 := IrrelevantLeftIdealForCAP( P1xP1 );
-#! <An object in Category of f.p. graded left
-#! modules over Q[x_1,x_2,x_3,x_4] (with weights
-#! [ [ 0, 1 ], [ 1, 0 ], [ 1, 0 ], [ 0, 1 ] ])>
-module2 := TurnIntoOldGradedModule( irP1xP1 );
-#! <A graded left module presented by 4 relations for 4 generators>
-module3 := TurnIntoCAPGradedModule( module2 );
-#! <An object in Category of f.p. graded left
-#! modules over Q[x_1,x_2,x_3,x_4] (with weights
-#! [ [ 0, 1 ], [ 1, 0 ], [ 1, 0 ], [ 0, 1 ] ])>
-module3 = irP1xP1;
-#! true
-#! @EndExample
-
-
-
 #! @Subsection Approximation of 0-th sheaf cohomology
 
 #! @Example
--- SheafCohomologyOnToricVarieties/gap/CohomologyTools.gd
+++ SheafCohomologyOnToricVarieties/gap/CohomologyTools.gd
@@ -2,54 +2,12 @@
 ##
 ##  CohomologyTools.gd          SheafCohomologyOnToricVarieties package
 ##
-##  Copyright 2019              Martin Bies,       ULB Brussels
+##  Copyright 2020              Martin Bies,       University of Oxford
 ##
 #! @Chapter Tools for cohomology computations
 ##
 ################################################################################################
 
-
-
-##############################################################################################
-##
-#! @Section Turn CAP Graded Modules into old graded modules and vice versa
-##
-##############################################################################################
-
-#! @Description
-#! The argument is a graded left or right module presentation M for CAP
-#! @Returns the corresponding graded modules in terms of the 'old' packages GradedModules
-#! @Arguments M
-DeclareOperation( "TurnIntoOldGradedModule",
-                  [ IsFpGradedLeftOrRightModulesObject ] );
-
-DeclareOperation( "TurnIntoCAPGradedModule",
-                  [ IsGradedModuleOrGradedSubmoduleRep ] );
-
-
-##############################################################################################
-##
-#! @Section Save CAP f.p. graded module to file
-##
-##############################################################################################
-
-#! @Description
-#! The argument is a graded left or right module presentation M for CAP and saves this module 
-#! to file as 'old' graded module presentation. By default, the files are saved in the main 
-#! directory of the package 'SheafCohomologyOnToricVarieties'.
-#! @Returns true (in case of success) or raises error in case the file could not be written
-#! @Arguments M
-DeclareOperation( "SaveToFileAsOldGradedModule",
-                  [ IsString, IsFpGradedLeftOrRightModulesObject ] );
-
-#! @Description
-#! The argument is a graded left or right module presentation M for CAP and saves this module 
-#! to file as CAP graded module presentation. By default, the files are saved in the main 
-#! directory of the package 'SheafCohomologyOnToricVarieties'.
-#! @Returns true (in case of success) or raises error in case the file could not be written
-#! @Arguments M
-DeclareOperation( "SaveToFileAsCAPGradedModule",
-                  [ IsString, IsFpGradedLeftOrRightModulesObject ] );
 
 
 ##############################################################################################
