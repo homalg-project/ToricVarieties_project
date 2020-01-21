8dfba5a46805f0211f76b62766bbcd1ff9661588
Martin Bies <MBies87@googlemail.com>
2020-01-21 15:50:43 +0000

    Add documentation on new method H0ParallelBySpasm

--- SheafCohomologyOnToricVarieties/gap/CohomologyWithSpasm.gd
+++ SheafCohomologyOnToricVarieties/gap/CohomologyWithSpasm.gd
@@ -8,6 +8,18 @@
 ##
 #########################################################################################
 
+#############################################################
+##
+#! @Section Cohomology from Spasm and Singular
+##
+#############################################################
+
+#! @Description
+#! Given a variety $V$ and an f.p. graded $S$-module $M$ ($S$ being
+#! the Cox ring of $V$), this method computes $H^0( V, \tilde{M} )$.
+#! It uses a combination of Singular and Spasm to perform this task.
+#! @Returns a vector space
+#! @Arguments V, M
 DeclareOperation( "H0ParallelBySpasm",
                   [ IsToricVariety, IsFpGradedLeftModulesObject ] );
 
