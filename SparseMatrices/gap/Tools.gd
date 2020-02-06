fe1a4fc3c7d0e23ef216852e648b4121c961cdc8
Martin Bies <MBies87@googlemail.com>
2020-02-06 11:59:33 +0000

    Remove declaration of SetSmastoDirectory

--- SparseMatrices/gap/Tools.gd
+++ SparseMatrices/gap/Tools.gd
@@ -24,13 +24,3 @@
 #! @Returns the corresponding directory
 #! @Arguments none
 DeclareOperation( "FindSmastoDirectory", [ ] );
-
-#! @Description
-#! This operation sets and stores the location of the smasto programs.
-#! In future operations, the method FindSmastoDirectory will use this path.
-#! If you wish to alter this path later, call this function again
-#! with the new path. If for example Smasto is installed at /home/person/smasto
-#! then the input for this function should be "/home/person/smasto".
-#! @Returns the corresponding directory
-#! @Arguments none
-DeclareOperation( "SetSmastoDirectory", [ IsString ] );
