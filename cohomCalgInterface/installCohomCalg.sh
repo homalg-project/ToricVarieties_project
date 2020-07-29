31ddad40170ed752a60b4d6a0598e0e6f6bf0ef7
Martin Bies <MBies87@googlemail.com>
2020-07-29 13:27:29 +0200

    Add scripts to build and remove cohomCalg locally
    Rather than shipping a binary

---
+++ cohomCalgInterface/installCohomCalg.sh
@@ -0,0 +1,32 @@
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Welcome!"
+echo "This script attempts to clone the cohomCalg repository and install it"
+echo "(https://github.com/BenjaminJurke/cohomCalg)"
+echo "------------------------------------------------------------------------"
+echo ""
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Step1: Clone cohomCalg"
+echo "------------------------------------------------------------------------"
+echo ""
+
+git clone https://github.com/HereAround/cohomCalg
+cd cohomCalg
+git checkout -b prepared
+git pull origin prepared
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Step2: Build cohomCalg"
+echo "------------------------------------------------------------------------"
+echo ""
+
+make
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "Installation complete"
+echo "------------------------------------------------------------------------"
+echo ""
