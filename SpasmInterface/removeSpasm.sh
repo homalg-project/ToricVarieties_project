73c2cdfab4b71fadd821652f9011f4ca5b6eca80
Martin Bies <MBies87@googlemail.com>
2021-03-10 10:50:53 -0500

    Improve remove script

--- SpasmInterface/removeSpasm.sh
+++ SpasmInterface/removeSpasm.sh
@@ -20,13 +20,9 @@ echo "Step2: Unconfigure SpasmInterface"
 echo "------------------------------------------------------------------------"
 echo ""
 
-touch set.gi
-echo 'LoadPackage( "SpasmInterface" );' >> set.gi
-echo 'SetSpasmDirectory( "" );' >> set.gi
-echo 'QUIT;;' >> set.gi
-gap set.gi
-rm set.gi
-
+cd gap
+> SpasmDirectory.txt
+cd ..
 
 echo ""
 echo "------------------------------------------------------------------------"
