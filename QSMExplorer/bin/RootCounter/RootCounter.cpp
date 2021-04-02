53dc76d7d7ae13096977cb0aa1b61ebac6399757
Martin Bies <MBies87@googlemail.com>
2021-04-01 22:01:34 -0400

    Add RootCounter

---
+++ QSMExplorer/bin/RootCounter/RootCounter.cpp
@@ -0,0 +1,240 @@
+//#include <algorithm>
+//#include<iostream>
+//#include <vector>
+//#include <numeric>
+//#include <stdexcept>
+//#include <thread>
+//#include <functional>
+//#include <mutex>
+//#include <chrono>
+//#include <stack>
+//#include <iterator>
+#include "WDiagram.h"
+
+//############################################################
+//#################### (5) Count roots #######################
+//############################################################
+
+
+// Global variable for counting diagrams // thread-safe addition
+unsigned long long int total;
+std::mutex myMutex;
+void IncreaseTotal( int s )
+{
+    std::lock_guard<std::mutex> guard(myMutex);
+    total = total + s;
+}
+
+
+// Count the minimal roots
+// Count the minimal roots
+int RootCounter( WeightedDiagram dia, int w0 )
+{
+    
+    // save total number of roots found
+    unsigned long long int sum = 0;
+    
+    // Upper bound for the number of weights to be set
+    int number_weights = dia.get_edges().size();
+    
+    // Create struc to record each snapshot
+    struct SnapShotStruct {
+        std::vector<int> w;
+        int k;
+    };
+    
+    // Create the stack
+    std::stack<SnapShotStruct> snapshotStack;
+    
+    // Initialize the first snapshot and add it to the stack
+    SnapShotStruct currentSnapshot;
+    std::vector<int> newWeights ( number_weights, 0 );
+    newWeights[ 0 ] = w0;
+    currentSnapshot.w = newWeights;
+    currentSnapshot.k = 1;
+    snapshotStack.push(currentSnapshot);
+    
+    // initialize a copy of dia
+    WeightedDiagram new_dia = dia;
+    
+    // Run...
+    while(!snapshotStack.empty())
+    {
+        
+        // Pick the top snapshot and delete it from the stack
+        currentSnapshot=snapshotStack.top();
+        snapshotStack.pop();
+        
+        // check what to do with this diagram
+        if ( currentSnapshot.k < number_weights ){
+            
+            // there are still weighs to be set -- try all placements
+            for ( int i = 1; i < dia.get_root(); i++ ){
+                
+                std::vector<int> new_weights = currentSnapshot.w;
+                new_weights[ currentSnapshot.k ] = i;
+                bool ok = dia.test_weights( new_weights );
+                if ( ok == true ){
+                
+                    // create new snapshot
+                    SnapShotStruct newSnapshot;
+                    newSnapshot.w = new_weights;
+                    newSnapshot.k = currentSnapshot.k + 1;
+                    
+                    // add snapshot to the stack
+                    snapshotStack.push(newSnapshot);
+                    
+                }
+                
+            }
+            
+        }
+        else{
+            
+            // set the weights in a new diagram
+            new_dia = dia;
+            int mult = new_dia.get_mult( currentSnapshot.w );
+            sum = sum + mult;
+            
+        }
+        
+    }
+    
+    // increase the total number of diagrams found and inform that this thread is finished
+    return sum;
+    
+}
+
+void Distributer( WeightedDiagram dia, std::vector<int> ws )
+{
+    
+    // initialize sum
+    unsigned long long int sum = 0;
+    
+    // find roots for different weight assignments
+    for ( int i = 0; i < ws.size(); i++ ){
+        sum = sum + RootCounter( dia, ws[ i ] );
+    }
+    
+    // increase the total number of roots found
+    IncreaseTotal( sum );
+    
+    // and signal end of thread run
+    std::cout<<"Thread complete. Total roots found: " << sum << "\n";
+    
+}
+
+// helper to print vector (of vectors) of integers
+void Myprint_vector(std::vector<int> const &vs, std::string const &message)
+{
+    std::cout << message;
+    for ( int i = 0; i < vs.size()-1; i++ ){
+        std::cout << vs[ i ] << ",";
+    }
+    std::cout << vs[ vs.size() - 1 ] << "\n";
+}
+
+// helper to print vector (of vectors) of integers
+void Myprint_vector_without_break(std::vector<int> const &vs, std::string const &msg1, std::string const &msg2 )
+{
+    std::cout << msg1;
+    for ( int i = 0; i < vs.size()-1; i++ ){
+        std::cout << vs[ i ] << ",";
+    }
+    std::cout << vs[ vs.size() - 1 ] << msg2;
+}
+
+void Myprint_vector_of_vector(std::vector<std::vector<int>> const &vs, std::string const &message)
+{
+    std::cout << message;
+    for ( int i = 0; i < vs.size(); i++ ){
+            Myprint_vector_without_break( vs[ i ], "(", ") " );
+    }
+    std::cout << "\n";
+}
+
+int countMinimalRoots( WeightedDiagram dia, int NUM_THREADS )
+{
+    
+    if ( NUM_THREADS <= 0 ){
+        throw std::invalid_argument( "Number of threads must be at least 1." );
+        return -1;
+    }
+    
+    // inform about the diagram in question
+    dia.print_complete_information();
+    
+    // initialize total as 0
+    total = 0;
+    
+    // take time for counting
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    
+    // inform what we are about to do
+    std::cout<<"Executing...\n";
+    std::cout<<"------------\n";
+    
+    // distribute weight assignments on first edge into NUM_THREADS packages -- each for one thread
+    std::vector<std::vector<int>> packages;
+    if ( NUM_THREADS >= dia.get_root() - 1 ){
+        for ( int i = 0; i < dia.get_root() - 1; i++ ){
+            std::vector<int> package ( 1, i+1 );
+            packages.push_back( package );
+        }
+    }
+    else{
+        
+        int package_size = (int) ( dia.get_root() - 1 ) / NUM_THREADS;
+        int current_root = 1;
+        for ( int i = 0; i < NUM_THREADS-1; i++ ){
+            
+            // set up thread with given package size
+            std::vector<int> package ( package_size );
+            for ( int j = 0; j < package_size; j++ ){
+                package[ j ] = current_root;
+                current_root++;
+            }
+            
+            // update package size
+            package_size = (int) ( dia.get_root() - current_root ) / ( NUM_THREADS - (i+1) );
+            
+            // append to packages
+            packages.push_back( package );
+            
+        }
+        
+        // set up thread with given package size
+        std::vector<int> package;
+        for ( int j = current_root; j < dia.get_root(); j++ ){
+            package.push_back( current_root );
+            current_root++;
+        }
+        packages.push_back( package );
+        
+    }
+    
+    // signal and start the threads
+    std::cout<<"Wait for " << packages.size() << " worker threads \n";
+    std::vector<std::thread> threadList;
+    for (int i = 0; i < packages.size(); i++)
+    {
+        WeightedDiagram new_dia = dia;
+        threadList.push_back( std::thread( Distributer, new_dia, packages[ i ] ) );
+    }
+    
+    // Now wait for the results of the worker threads (i.e. call the join() function on each of the std::thread objects) and inform the user
+    std::for_each(threadList.begin(),threadList.end(), std::mem_fn(&std::thread::join));
+    std::cout << "Found " << total << " minimal roots\n\n";
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time for run: " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]\n\n";
+    
+    // return the number of diagrams
+    return total;
+    
+}
+
+// default/convenience method
+int countMinimalRoots( WeightedDiagram dia )
+{
+    return countMinimalRoots( dia, dia.get_root() - 1 );
+}
---
+++ QSMExplorer/bin/RootCounter/WDiagram.h
@@ -0,0 +1,66 @@
+// A program to compute the number of minimal limit roots on full blowups of nodal curves
+
+#include<iostream>
+#include <vector>
+#include <numeric>
+#include <stdexcept>
+
+#ifndef WDIAGRAM_H
+#define WDIAGRAM_H
+
+// Define a class for weighted diagrams
+// Define a class for weighted diagrams
+
+class WeightedDiagram
+{
+    
+    // variables that have to be set upon construction
+    std::vector<int> vertices;
+    std::vector<int> degrees;
+    std::vector<int> genera;
+    std::vector<std::vector<int>> edges;
+    int genus;
+    int root;
+    
+    // variables that are computed upon construction
+    int degree;
+    int h0_min;
+    std::vector<int> edge_numbers;
+    
+    // the public section of this class
+    public:
+    
+    // Constructors
+    WeightedDiagram();
+    WeightedDiagram(std::vector<int> &vi, std::vector<int> &di, std::vector<int> &gi, std::vector<std::vector<int>> &ei, int &g, int &r );
+    
+    // print functions
+    void print_vertices();
+    void print_edge_numbers();
+    void print_degrees();
+    void print_genera();
+    void print_edges();
+    void print_degree();
+    void print_root();
+    void print_genus();
+    void print_h0_min();
+    void print_complete_information();
+    
+    // getter functions
+    std::vector<int> get_vertices();
+    std::vector<int> get_edge_numbers();
+    std::vector<int> get_degrees();
+    std::vector<int> get_genera();
+    std::vector<std::vector<int>> get_edges();
+    int get_degree();
+    int get_root();
+    int get_genus();
+    int get_h0_min();
+    int get_mult(std::vector<int> weights);
+    
+    // set and test weight configurations
+    bool test_weights(std::vector<int> weights);
+    
+};
+
+#endif
