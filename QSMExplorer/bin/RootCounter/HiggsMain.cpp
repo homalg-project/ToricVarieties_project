5e12a60ecd2ea4b9cdd10369efb36eb60ac07171
Martin Bies <MBies87@googlemail.com>
2021-10-05 18:11:23 -0400

    Perform C++ scan over outfluxes in parallel

--- QSMExplorer/bin/RootCounter/HiggsMain.cpp
+++ QSMExplorer/bin/RootCounter/HiggsMain.cpp
@@ -11,11 +11,14 @@
 #include <thread>
 #include <vector>
 
+// guard for thread-safe operations
+std::mutex myMutexFlex;
+
 #include "RootDistributionCounter.cpp"
+#include "Scanner.cpp"
 #include "Cartesian.cpp"
 #include "Partitions.cpp"
 #include "HiggsDistributionCounter.cpp"
-
 
 // Optimizations for speedup
 #pragma GCC optimize("Ofast")
@@ -46,7 +49,7 @@ int main(int argc, char* argv[]) {
     // (3) degrees_H2 (std::vector<int>)
     // (4) genera (std::vector<int>)
     // (5) edges (std::vector<std::vector<int>>)
-    // (6) external_legs (std::vector<int>)
+    // (6) legs_per_component (std::vector<int>)
     // (7) genus (int)
     // (8) root (int)
     // (9) number of threads (int) - optional
@@ -55,14 +58,19 @@ int main(int argc, char* argv[]) {
     // So expect the following input:
     // { #Vertices, degrees, genera, #Edges, edge-information, genus, root, threads }
     
+    // TODO Handle number of threads better!
+    
     // convert the input data accordingly
     int numberVertices = input[ 0 ];
-    std::vector<int> vertices, degrees_H1, degrees_H2, genera;
+    std::vector<int> vertices( numberVertices );
+    std::vector<int> degrees_H1( numberVertices );
+    std::vector<int> degrees_H2( numberVertices );
+    std::vector<int> genera( numberVertices );
     for ( int i = 1; i <= numberVertices; i++ ){
-        vertices.push_back( i - 1 );
-        degrees_H1.push_back( input[ i ] );
-        degrees_H2.push_back( input[ i ] + numberVertices );
-        genera.push_back( input[ i + 2 * numberVertices ] );
+        vertices[ i - 1 ] = i - 1;
+        degrees_H1[ i - 1 ] = input[ i ];
+        degrees_H2[ i - 1] = input[ i + numberVertices ];
+        genera[ i - 1 ] = input[ i + 2 * numberVertices ];
     }
     int numberEdges = input[ 3 * numberVertices + 1 ];
     std::vector<std::vector<int>> edges;
@@ -73,29 +81,53 @@ int main(int argc, char* argv[]) {
         helper[ 1 ] = input[ index + 1 ];
         edges.push_back( helper );
     }
-    int numberExternalEdges = input[ 3 * numberVertices + 1 + 1 + 2 * numberEdges ];
+    std::vector<int> legs_per_component;
     std::vector<int> external_legs;
-    for ( int i = 0; i < numberExternalEdges; i ++ ){
-        external_legs.push_back( input[ 3 * numberVertices + 1 + 1 + 2 * numberEdges + 1 + i ] );
+    for ( int i = 0; i < numberVertices; i ++ ){
+        legs_per_component.push_back( input[ 3 * numberVertices + 1 + 1 + 2 * numberEdges + i ] );
+        for ( int j = 0; j < input[ 3 * numberVertices + 1 + 1 + 2 * numberEdges + i ]; j++ ){
+            external_legs.push_back( i );
+        }
     }
-    int genus = input[ 3 * numberVertices + 3 + 2 * numberEdges + numberExternalEdges + 1 ];
-    int root = input[ 3 * numberVertices + 3 + 2 * numberEdges + numberExternalEdges + 2 ];
-    int number_threads = input[ 3 * numberVertices + 3 + 2 * numberEdges + numberExternalEdges + 3 ];
-    int h0Max = input[ 3 * numberVertices + 3 + 2 * numberEdges + numberExternalEdges + 4 ];
-    int details = input[ 3 * numberVertices + 3 + 2 * numberEdges + numberExternalEdges + 5 ];
+    int genus = input[ 4 * numberVertices + 2 * numberEdges + 2 ];
+    int root = input[ 4 * numberVertices + 2 * numberEdges + 3 ];
+    int number_threads = input[ 4 * numberVertices + 2 * numberEdges + 4 ];
+    int h0Max = input[ 4 * numberVertices + 2 * numberEdges + 5 ];
+    int details = input[ 4 * numberVertices + 2 * numberEdges + 6 ];
     bool display_details = true;
     if ( details < 0 ){
             display_details = false;
     }
     
-    // find the number of legs per component
-    std::sort( external_legs.begin(), external_legs.end() );
-    std::vector<int> legs_per_component( vertices.size(), 0 );
-    for ( int i = 0; i < external_legs.size(); i++ ){
-        legs_per_component[ external_legs[ i ] ]++;
+    // Display what we obtained
+    if ( display_details ){
+        std:: cout << "\n";
+        std:: cout << "Received the following data:\n";
+        std:: cout << "----------------------------------------\n";
+        std::cout << "Degrees H1: ";
+        for ( int j = 0; j < degrees_H1.size(); j++ ){
+            std::cout << degrees_H1[ j ] << ", ";
+        }
+        std::cout << "\n";
+        std::cout << "Degrees H2: ";
+        for ( int j = 0; j < degrees_H2.size(); j++ ){
+            std::cout << degrees_H2[ j ] << ", ";
+        }
+        std::cout << "\n";
+        std::cout << "Root: " << root << "\n";
+        std::cout << "External legs: (";
+        for ( int i = 0; i < external_legs.size() - 1; i++ ){
+            std::cout << external_legs[ i ] << ", ";
+        }
+        std::cout << external_legs[ external_legs.size() - 1 ] << ")\n";
+        std::cout << "Legs per component: (";
+        for ( int i = 0; i < numberVertices; i++ ){
+            std::cout << legs_per_component[ i ] << ", ";
+        }
+        std::cout << ")\n\n";
     }
     
-    // check if curve class and bundle class are of correct length
+    // Check if curve class and bundle class are of correct length
     if ( ( genus < 0 ) ){
         if ( display_details ){
             std::cout << "Genus must not be negative.\n";
@@ -115,8 +147,15 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     
+    if ( display_details ){
+        std::cout << "Find minimal and maximal outfluxes...\n";
+    }
+    
+    
     
     // (1) Construct all outfluxes values
+    // (1) Construct all outfluxes values
+    
     int min_outflux, max_outflux;
     std::vector<std::vector<int>> outflux_values;
     for ( int i = 0; i < vertices.size(); i++ ){
@@ -124,6 +163,9 @@ int main(int argc, char* argv[]) {
         // find minimal and maximal outfluxes
         min_outflux = legs_per_component[ i ];
         max_outflux = legs_per_component[ i ] * ( root - 1 );
+        if ( display_details ){
+            std::cout << "Component " << i << ": (" << min_outflux << ", " << max_outflux << ")\n";
+        }
         
         // construct the possible flux values explicitly
         std::vector<int> possible_values;
@@ -134,9 +176,13 @@ int main(int argc, char* argv[]) {
         // and add this list of values to outflux_values
         outflux_values.push_back( possible_values );
     }
+    std::cout << "\n";
+    
     
     
     // (2) Cartesian product to identify all outflux values (and cast it into unsigned long long ints)
+    // (2) Cartesian product to identify all outflux values (and cast it into unsigned long long ints)
+    
     std::vector<std::vector<int>> cartesian = product( outflux_values );
     std::vector<std::vector<unsigned long long int>> all_outfluxes( cartesian.size() );
     for ( int i = 0; i < cartesian.size(); i++ ){
@@ -147,110 +193,23 @@ int main(int argc, char* argv[]) {
         all_outfluxes[ i ] = dummy;
     }
     
-    // (3) Iterate over all outfluxes to gather data to be pieced together
-    // TODO: Perform in parallel threads
+    
+    // (3) Scan all outfluxes to compute "min-distributions"
+    // (3) Scan all outfluxes to compute "min-distributions"
+    if ( display_details ){
+        std::cout << "Total number of outfluxes to be studied: " << all_outfluxes.size() << "\n\n";
+        if ( number_threads > 0 ){
+            std::cout << "Perform scan in " << number_threads << " parallel threads...\n";
+        }
+        else {
+            std::cout << "Perform scan in single threads...\n\n";
+        }
+    }
+
+    // (3.1) Prepare scan
     std::vector<std::vector<unsigned long long int>> outfluxes_H1, outfluxes_H2;
     std::vector<std::vector<unsigned long long int>> dist_H1, dist_H2;
     int h0MinUsed;
-    for ( int i = 0; i < all_outfluxes.size(); i++ ){
-        
-        // (3.1) Generate weights for the outflux
-        std::vector<int> external_weights( external_legs.size() );
-        int iterator = 0;
-        for ( int j = 0; j < vertices.size(); j++ ){
-            
-            int flux_for_component = all_outfluxes[ i ][ j ];
-            int leg_number = legs_per_component[ j ];
-            
-            for ( int k = 0; k < leg_number; k++ ){
-                external_weights[ iterator ] = flux_for_component / leg_number;
-                flux_for_component = flux_for_component - ( flux_for_component / leg_number );
-                leg_number = leg_number - 1;
-            }
-            
-        }
-        
-        // (3.2) Construct weighted H1-diagram
-        WeightedDiagramWithExternalLegs dia_H1 = WeightedDiagramWithExternalLegs( vertices, degrees_H1, genera, edges, external_legs, external_weights, genus, root );
-        
-        // (3.3) Only proceed if situation is not automatically degenerate
-        h0MinUsed = dia_H1.get_h0_min();
-        if ( h0Max >= h0MinUsed ){
-            
-            // Compute distribution
-            std::vector<unsigned long long int> n( h0Max - h0MinUsed + 1, 0 );
-            if ( number_threads > 0 ){
-                countRootDistribution( dia_H1, number_threads, h0MinUsed, h0Max, n, display_details );
-            }
-            else{
-                countRootDistribution( dia_H1, h0MinUsed, h0Max, n, display_details );
-            }
-            
-            // Check if distribution is not trivial
-            bool zeros = std::all_of( n.begin(), n.end(), [](int j) { return j==0; } );
-            
-            // Append if the result is not trivial
-            if ( ! zeros ){
-                
-                // Prepare result
-                std::vector<unsigned long long int> dist( h0Max + 1, 0 );
-                for ( int j = 0; j < n.size(); j++ ){
-                    dist[ j + h0MinUsed ] = n[ j ];
-                }
-                
-                // And safe it
-                outfluxes_H1.push_back( all_outfluxes[ i ] );
-                dist_H1.push_back( dist );
-
-            }
-            
-        }
-        
-        // (3.4) Construct weighted H2-diagram
-        WeightedDiagramWithExternalLegs dia_H2 = WeightedDiagramWithExternalLegs( vertices, degrees_H2, genera, edges, external_legs, external_weights, genus, root );
-        
-        // (3.5) Only proceed if situation is not automatically degenerate
-        h0MinUsed = dia_H2.get_h0_min();
-        if ( h0Max >= h0MinUsed ){
-            
-            // Compute distribution
-            std::vector<unsigned long long int> n( h0Max - h0MinUsed + 1, 0 );
-            if ( number_threads > 0 ){
-                countRootDistribution( dia_H2, number_threads, h0MinUsed, h0Max, n, display_details );
-            }
-            else{
-                countRootDistribution( dia_H2, h0MinUsed, h0Max, n, display_details );
-            }
-            
-            // Check if distribution is not trivial
-            bool zeros = std::all_of( n.begin(), n.end(), [](int j) { return j==0; } );
-            
-            // Append if the result is not trivial
-            if ( ! zeros ){
-                
-                // Prepare distribution
-                std::vector<unsigned long long int> dist( h0Max + 1, 0 );
-                for ( int j = 0; j < n.size(); j++ ){
-                    dist[ j + h0MinUsed ] = n[ j ];
-                }
-                
-                // And safe it
-                outfluxes_H2.push_back( all_outfluxes[ i ] );
-                dist_H2.push_back( dist );
-
-            }
-            
-        }
-        
-    }
-    
-    // (4) Prepare to piece local data together
-    int number_components = vertices.size();
-    std::vector<int> legs_per_component_halved( legs_per_component );
-    for ( int i = 0; i < legs_per_component.size(); i++ ){
-        legs_per_component_halved[ i ] = legs_per_component[ i ] / 2;
-    }
-    std::vector<unsigned long long int> final_dist( 3 * h0Max , 0 );
     std::vector<int> status;
     if ( number_threads > 0 ){
         for ( int i = 0; i < number_threads; i++ ){
@@ -261,8 +220,8 @@ int main(int argc, char* argv[]) {
         status.push_back( 0 );
     }
     
-    // (5) Partition workload to threads
-    int package_size = outfluxes_H1.size() / number_threads;
+    // (3.2) Partition workload and start threads
+    int package_size = all_outfluxes.size() / number_threads;
     std::vector<std::thread> threadList;
     int start, stop;
     for (int i = 0; i < number_threads; i++)
@@ -277,7 +236,71 @@ int main(int argc, char* argv[]) {
         std::cout << "Start thread " << i << "\n";
         
         // Start the worker threads
-        threadList.push_back( std::thread(   compute_distribution, 
+        threadList.push_back( std::thread(   FluxScanner, 
+                                                                    all_outfluxes,
+                                                                    std::ref( outfluxes_H1 ),
+                                                                    std::ref( outfluxes_H2 ),
+                                                                    std::ref( dist_H1 ),
+                                                                    std::ref( dist_H2 ),
+                                                                    start,
+                                                                    stop,
+                                                                    std::ref( status ),
+                                                                    i,
+                                                                    legs_per_component,
+                                                                    root,
+                                                                    vertices,
+                                                                    degrees_H1,
+                                                                    degrees_H2,
+                                                                    genera,
+                                                                    edges,
+                                                                    external_legs,
+                                                                    genus,
+                                                                    number_threads,
+                                                                    h0Max
+                                                                    ) );
+        
+    }
+    std::for_each(threadList.begin(),threadList.end(), std::mem_fn(&std::thread::join));
+    
+    // Inform what we have achieved
+    if ( display_details ){
+        std::cout << "\n\n";
+        std::cout << "Outfluxes H1: " << outfluxes_H1.size() << ", " << dist_H1.size() << "\n";
+        std::cout << "Outfluxes H2: " << outfluxes_H2.size() << ", " << dist_H2.size() << "\n";
+        std::cout << "Now start to piece the local data together... \n\n";
+    }
+    
+    // (4) Prepare to piece local data together
+    std::vector<int> legs_per_component_halved( legs_per_component );
+    for ( int i = 0; i < legs_per_component.size(); i++ ){
+        legs_per_component_halved[ i ] = legs_per_component[ i ] / 2;
+    }
+    std::vector<unsigned long long int> final_dist( 3 * h0Max , 0 );
+    if ( number_threads > 0 ){
+        for ( int i = 0; i < number_threads; i++ ){
+            status.push_back( 0 );
+        }
+    }
+    else{
+        status.push_back( 0 );
+    }
+    
+    // (5) Partition workload and start threads
+    package_size = outfluxes_H1.size() / number_threads;
+    std::vector<std::thread> threadList2;
+    for (int i = 0; i < number_threads; i++)
+    {
+
+        // Find start and stop position for thread
+        start = i * package_size;
+        if ( i < number_threads - 1 ){ stop = ( i + 1 ) * package_size - 1; }
+        else{ stop = (int) outfluxes_H1.size() -1; }
+        
+        // Signal that we start this thread
+        std::cout << "Start thread " << i << "\n";
+        
+        // Start the worker threads
+        threadList2.push_back( std::thread(   compute_distribution, 
                                                                     outfluxes_H1,
                                                                     outfluxes_H2,
                                                                     dist_H1,
@@ -292,11 +315,9 @@ int main(int argc, char* argv[]) {
                                                                 ) );
         
     }
+    std::for_each(threadList2.begin(),threadList2.end(), std::mem_fn(&std::thread::join));
     
-    // (6) Wait for all threads to complete
-    std::for_each(threadList.begin(),threadList.end(), std::mem_fn(&std::thread::join));
-    
-    // (7) Print result
+    // (6) Print result
     std::cout << "##############\n";
     std::cout << "Found root distribution:\n";
     std::cout << "##############\n";
@@ -304,7 +325,7 @@ int main(int argc, char* argv[]) {
         std::cout << "H0 = " << i << ":\t" << final_dist[ i ] << "\n";
     }
     
-    // (8) Signal success
+    // (7) Signal success
     return 0;
     
 }
---
+++ QSMExplorer/bin/RootCounter/Scanner.cpp
@@ -0,0 +1,158 @@
+void UpdateCollectedData( std::vector<std::vector<unsigned long long int>>& collector1,
+                                            std::vector<std::vector<unsigned long long int>>& collector2,
+                                            std::vector<unsigned long long int> & change1,
+                                            std::vector<unsigned long long int> & change2 )
+{
+    
+    std::lock_guard<std::mutex> guard(myMutexFlex);
+    collector1.push_back( change1 );
+    collector2.push_back( change2 );
+    
+}
+
+void UpdateStatus( std::vector<int>& status, int thread_number, int progress, int numb_H1_outfluxes, int numb_H2_outfluxes )
+{
+    
+    std::lock_guard<std::mutex> guard(myMutexFlex);
+    status[ thread_number ] = progress;
+    std::string output = "Status [%]: (";    
+    for ( int i = 0; i < status.size() - 1; i ++ ){
+        output = output + std::__cxx11::to_string( status[ i ] ) + ", ";
+    }
+    output = output + std::__cxx11::to_string( status[ status.size() - 1 ] ) + ") -- ( " + std::__cxx11::to_string( numb_H1_outfluxes ) + ", " + std::__cxx11::to_string( numb_H2_outfluxes ) + ")";
+    std::cout << output << "\r" << std::flush;
+    
+}
+
+void FluxScanner(    std::vector<std::vector<unsigned long long int>> all_outfluxes,
+                                std::vector<std::vector<unsigned long long int>> & outfluxes_H1, 
+                                std::vector<std::vector<unsigned long long int>> & outfluxes_H2,
+                                std::vector<std::vector<unsigned long long int>> & dist_H1,
+                                std::vector<std::vector<unsigned long long int>> & dist_H2,
+                                int start,
+                                int stop,
+                                std::vector<int> & status,
+                                int thread_number,
+                                std::vector<int> legs_per_component,
+                                int root,
+                                std::vector<int> vertices,
+                                std::vector<int> degrees_H1,
+                                std::vector<int> degrees_H2,
+                                std::vector<int> genera,
+                                std::vector<std::vector<int>> edges,
+                                std::vector<int> external_legs,
+                                int genus,
+                                int number_threads,
+                                int h0Max
+                )
+{
+
+    int number_sub_threads = 1;
+    int h0MinUsed;
+    int progress = 0;
+    int number_external_legs = std::accumulate( legs_per_component.begin(), legs_per_component.end(), 0 );
+    int total_degree_H1 = std::accumulate( degrees_H1.begin(), degrees_H1.end(), 0 );
+    int total_degree_H2 = std::accumulate( degrees_H2.begin(), degrees_H2.end(), 0 );
+        
+    for ( int i = start; i < stop; i++ ){
+
+        // (3.0) Display status
+        if ( i * 100 / all_outfluxes.size() > progress ){
+            progress = ( i - start ) * 100 / ( stop - start );
+            UpdateStatus( status, thread_number, progress, outfluxes_H1.size(), outfluxes_H2.size() );
+        }
+        
+        
+        // (3.1) Compute total outflux
+        int total_flux = std::accumulate( all_outfluxes[ i ].begin(), all_outfluxes[ i ].end(), 0 );
+        
+        // (3.2) Generate weights for the outflux
+        std::vector<int> external_weights( number_external_legs );
+        int iterator = 0;
+        for ( int j = 0; j < legs_per_component.size(); j++ ){
+            
+            int flux_for_component = all_outfluxes[ i ][ j ];
+            int leg_number = legs_per_component[ j ];
+            int remaining_legs = leg_number;
+            
+            for ( int k = 0; k < leg_number; k++ ){
+                external_weights[ iterator ] = flux_for_component / remaining_legs;
+                flux_for_component = flux_for_component - ( flux_for_component / remaining_legs );
+                remaining_legs = remaining_legs - 1;
+                iterator = iterator + 1;
+            }
+            
+        }
+        
+        // (3.3) Compute corresponding distribution on H1
+        bool display = false;
+        if ( ( total_degree_H1 - total_flux ) % root == 0 ){
+            
+            // Construct weighted H1-diagram
+            WeightedDiagramWithExternalLegs dia_H1 = WeightedDiagramWithExternalLegs( vertices, degrees_H1, genera, edges, external_legs, external_weights, genus, root );
+            
+            // Only proceed if situation is not automatically degenerate
+            h0MinUsed = dia_H1.get_h0_min();
+            if ( h0Max >= h0MinUsed ){
+                
+                // Compute distribution
+                std::vector<unsigned long long int> n( h0Max - h0MinUsed + 1, 0 );
+                countRootDistribution( dia_H1, number_sub_threads, h0MinUsed, h0Max, n, display );
+                
+                // Check if distribution is not trivial
+                bool zeros = std::all_of( n.begin(), n.end(), [](int j) { return j==0; } );
+                
+                // Append if the result is not trivial
+                if ( ! zeros ){
+                    
+                    // Prepare result
+                    std::vector<unsigned long long int> dist( h0Max + 1, 0 );
+                    for ( int j = 0; j < n.size(); j++ ){
+                        dist[ j + h0MinUsed ] = n[ j ];
+                    }
+                    
+                    // And remember this result
+                    UpdateCollectedData( outfluxes_H1, dist_H1, all_outfluxes[ i ], dist );
+                    
+                }
+                
+            }
+        }
+        
+        // (3.4) Compute corresponding distribution on H2
+        if ( ( total_degree_H2 - total_flux ) % root == 0 ){
+            
+            // Construct weighted H2-diagram
+            WeightedDiagramWithExternalLegs dia_H2 = WeightedDiagramWithExternalLegs( vertices, degrees_H2, genera, edges, external_legs, external_weights, genus, root );
+            
+            // Only proceed if situation is not automatically degenerate
+            if ( h0Max >= h0MinUsed ){
+                
+                // Compute distribution
+                std::vector<unsigned long long int> n( h0Max - h0MinUsed + 1, 0 );
+                countRootDistribution( dia_H2, number_sub_threads, h0MinUsed, h0Max, n, display );
+                
+                // Check if distribution is not trivial
+                bool zeros = std::all_of( n.begin(), n.end(), [](int j) { return j==0; } );
+                
+                // Append if the result is not trivial
+                if ( ! zeros ){
+                    
+                    // Prepare distribution
+                    std::vector<unsigned long long int> dist( h0Max + 1, 0 );
+                    for ( int j = 0; j < n.size(); j++ ){
+                        dist[ j + h0MinUsed ] = n[ j ];
+                    }
+                    
+                    // And remember this result
+                    UpdateCollectedData( outfluxes_H2, dist_H2, all_outfluxes[ i ], dist );
+                    
+                }
+            
+            }
+        
+        }
+    
+    }
+    
+}
