acb14391326288cc8a3fdf281fd352e9eb6bf742
Martin Bies <MBies87@googlemail.com>
2021-10-30 18:57:35 -0400

    Add gap support for new cpp routines

--- QSMExplorer/gap/ToolsHiggsCurve.gd
+++ QSMExplorer/gap/ToolsHiggsCurve.gd
@@ -29,11 +29,39 @@ DeclareOperation( "LimitRootDistributionForHiggsCurveInQSM", [ IsInt, IsInt ] );
 DeclareOperation( "LimitRootDistributionForHiggsCurveInQSM", [ IsInt, IsInt, IsBool ] );
 
 #! @Description
+#! This operation performs the divide step for the computation of limit root distribution on the Higgs curve in the i-th QSM.
+#! @Returns a list
+#! @Arguments 
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMDivideStep", [ IsInt, IsInt ] );
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMDivideStep", [ IsInt, IsInt, IsBool ] );
+
+#! @Description
+#! This operation performs the conquer step for the computation of limit root distribution on the Higgs curve in the i-th QSM.
+#! @Returns a list
+#! @Arguments 
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMConquerStep", [ IsInt, IsInt ] );
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMConquerStep", [ IsInt, IsInt, IsBool ] );
+
+#! @Description
 #! This operation returns a list corresponding to the limit root distribution on the Higgs curve in the QSM of the i-th polytope.
 #! @Returns a list
 #! @Arguments 
 DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMByPolytope", [ IsInt, IsInt ] );
 DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMByPolytope", [ IsInt, IsInt, IsBool ] );
+
+#! @Description
+#! This operation performs the divide step for the computation of limit root distribution on the Higgs curve in the QSM of the i-th polytope.
+#! @Returns a list
+#! @Arguments 
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMByPolytopeDivideStep", [ IsInt, IsInt ] );
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMByPolytopeDivideStep", [ IsInt, IsInt, IsBool ] );
+
+#! @Description
+#! This operation performs the conquer step for the computation of limit root distribution on the Higgs curve in the QSM of the i-th polytope.
+#! @Returns a list
+#! @Arguments 
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMByPolytopeConquerStep", [ IsInt, IsInt ] );
+DeclareOperation( "LimitRootDistributionForHiggsCurveInQSMByPolytopeConquerStep", [ IsInt, IsInt, IsBool ] );
 
 #! @Description
 #! This operation returns a list corresponding to the limit root distribution on the Higgs curve in the i-th QSM.
@@ -50,5 +78,9 @@ DeclareOperation( "LimitRootDistributionForRDQCurveInQSMByPolytope", [ IsInt, Is
 DeclareOperation( "LimitRootDistributionForRDQCurveInQSMByPolytope", [ IsInt, IsInt, IsBool ] );
 
 DeclareOperation( "LimitRootDistributionHiggs", [ IsRecord, IsInt, IsBool ] );
+DeclareOperation( "LimitRootDistributionHiggsDivide", [ IsRecord, IsInt, IsBool ] );
+DeclareOperation( "LimitRootDistributionHiggsConquer", [ IsRecord, IsInt, IsBool ] );
 
 DeclareOperation( "LimitRootDistributionAlongHiggsPhilosophy", [ IsList, IsBool ] );
+DeclareOperation( "LimitRootDistributionAlongHiggsPhilosophyDivide", [ IsList, IsBool ] );
+DeclareOperation( "LimitRootDistributionAlongHiggsPhilosophyConquer", [ IsList, IsBool ] );
