d47a3a930da6c271415faf88a33281c116c2c0e0
Martin Bies <MBies87@googlemail.com>
2022-02-26 15:02:48 -0500

    Organize code in tree_like_computation.cpp better

--- QSMExplorer/bin/PartialBlowups/Auxilliary/tree_like_computations.cpp
+++ QSMExplorer/bin/PartialBlowups/Auxilliary/tree_like_computations.cpp
@@ -55,10 +55,37 @@ int number_connected_components(const std::vector<std::vector<int>>& input_edges
 }
 
 
+// (1) Compute betti number
+// (1) Compute betti number
+// (1) Compute betti number
 
-// (1) Compute h0 on a CONNECTED tree-like rational curve (no check for connected conducted)
-// (1) Compute h0 on a CONNECTED tree-like rational curve (no check for connected conducted)
-// (1) Compute h0 on a CONNECTED tree-like rational curve (no check for connected conducted)
+int betti_number(const std::vector<std::vector<int>>& input_edges)
+{
+    
+    // (1) Find all vertices (avoiding duplicated) and sort them in ascending order
+    std::vector<int> vertices;
+    vertices.reserve(2*input_edges.size());
+    for (int i = 0; i < input_edges.size(); i++){
+        if (!(std::count(vertices.begin(), vertices.end(), input_edges[i][0]))) {
+            vertices.push_back(input_edges[i][0]);
+        }
+        if (!(std::count(vertices.begin(), vertices.end(), input_edges[i][1]))) {
+            vertices.push_back(input_edges[i][1]);
+        }
+    }
+    
+    // (2) Number of connected components
+    int ncc = number_connected_components(input_edges);
+    
+    // (3) return the betti number
+    return input_edges.size() + ncc - vertices.size();
+    
+}
+
+
+// (2) Compute h0 on a CONNECTED tree-like rational curve (no check for connected conducted)
+// (2) Compute h0 on a CONNECTED tree-like rational curve (no check for connected conducted)
+// (2) Compute h0 on a CONNECTED tree-like rational curve (no check for connected conducted)
 
 int h0_on_rational_tree(const std::vector<int>& vertices,
                                      const std::vector<int>& degrees,
@@ -195,15 +222,9 @@ int h0_on_rational_tree(const std::vector<int>& vertices,
 }
 
 
-
-
-
-
-
-
-// (2) Find the connected components of a graph
-// (2) Find the connected components of a graph
-// (2) Find the connected components of a graph
+// (3) Find the connected components of a graph
+// (3) Find the connected components of a graph
+// (3) Find the connected components of a graph
 
 void find_connected_components(const std::vector<std::vector<int>> & input_edges,
                                                     const std::vector<int> & degrees,
@@ -289,59 +310,6 @@ void find_connected_components(const std::vector<std::vector<int>> & input_edges
         std::cout << "############################################\n\n";
     }
 }
-
-
-
-
-
-// (3) Compute betti number
-// (3) Compute betti number
-// (3) Compute betti number
-
-int betti_number(const std::vector<std::vector<int>>& input_edges)
-{
-    
-    // (1) Find all vertices (avoiding duplicated) and sort them in ascending order
-    std::vector<int> vertices;
-    vertices.reserve(2*input_edges.size());
-    for (int i = 0; i < input_edges.size(); i++){
-        if (!(std::count(vertices.begin(), vertices.end(), input_edges[i][0]))) {
-            vertices.push_back(input_edges[i][0]);
-        }
-        if (!(std::count(vertices.begin(), vertices.end(), input_edges[i][1]))) {
-            vertices.push_back(input_edges[i][1]);
-        }
-    }
-    
-    // (2) Construct vertex_correspondence: input vertex names (e.g. "0, 2, 5, 6, ...") -> "0, 1, 2, 3, ..."
-    std::map<int, int> vertex_correspondence;
-    for (int i = 0; i < vertices.size(); i++){
-        vertex_correspondence.insert(std::pair<int, int>(vertices[i], i));
-    }
-    
-    // (3) Form list of edges with internal/new vertex indices (-> can be easily processed below)
-    std::vector<std::vector<int>> edges;
-    edges.reserve(input_edges.size());
-    for (int i = 0; i < input_edges.size(); i++){
-        edges.push_back({vertex_correspondence[input_edges[i][0]], vertex_correspondence[input_edges[i][1]]});
-    }
-    
-    // (4) Count number of connected components
-    int c = 0;
-    std::vector<int> parent(vertices.size());
-    std::iota(std::begin(parent), std::end(parent), 0);
-    for (int i = 0; i < edges.size(); i++){
-        parent[merge(parent, edges[i][0])] = merge(parent, edges[i][1]);
-    }
-    for (int i = 0; i < vertices.size(); i++) {
-        c += (parent[i] == i);
-    }
-    
-    // return betti number
-    return input_edges.size() + c - vertices.size();
-    
-}
-
 
 
 // (4) Compute/estimate via lower bound h0 on nodal curve with P1s as components
