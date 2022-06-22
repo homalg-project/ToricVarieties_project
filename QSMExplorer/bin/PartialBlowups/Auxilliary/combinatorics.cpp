d6b4a1a25d24cdb626208628305c89830b171370
Ross Hochwert <rosshochwert@gmail.com>
2022-06-21 22:00:17 -0400

    second attempt at hash

--- QSMExplorer/bin/PartialBlowups/Auxilliary/combinatorics.cpp
+++ QSMExplorer/bin/PartialBlowups/Auxilliary/combinatorics.cpp
@@ -4,58 +4,6 @@
 
 // Task: Given an integer N, partition it into a sum of n integers w_1,...,w_n. The k-th integer w_n can have values minima[k] <= k <= maxima[k].
 
-
-#include <unordered_map>
-#include <chrono>
-
-// Create a customer Key struct for lookup in our hash table
-// define equivalence 
-struct customKey {
-    int N;
-    int n;
-    std::vector<int> minima;
-    std::vector<int> maxima;
-
-    bool operator==(const customKey &other) const {
-        return (N == other.N && n == other.n && minima == other.minima && maxima == other.maxima);
-    }
-};
-
-// Create a custom key hasher for our table
-
-struct KeyHasher {
-    std::size_t operator()(const customKey& k) const {
-        using std::size_t;
-        using std::hash;
-        using std::string;
-
-        std::size_t h1 = std::hash<int>()(k.N);
-        std::size_t h2 = std::hash<int>()(k.n);
-        std::size_t h3 = k.minima.size();
-        std::size_t h4 = k.maxima.size();
-
-        for (auto &i : k.minima) {
-            h3 ^= i + 0x9e3779b9 + (h3 << 6) + (h3 >> 2);
-        }
-
-        for (auto &i : k.maxima) {
-            h4 ^= i + 0x9e3779b9 + (h4 << 6) + (h4 >> 2);
-        }
-
-        return h1^h2^h3^h4;
-
-    }
-};
-
-
-// Create our hash table for recording these combinations
-
-std::unordered_map<customKey, std::vector<std::vector<int>>, KeyHasher> hashTable = {};
-auto total_time = 0;
-auto original_time = 0;
-auto hashing_time = 0;
-int i = 0;
-
 void comp_partitions(
         const int & N,
         const int & n,
@@ -63,125 +11,67 @@ void comp_partitions(
         const std::vector<int> & maxima,
         std::vector<std::vector<int>> & partitions){
     
-
-    //createCustomKey
-
-    customKey k;
-    k.N = N;
-    k.n = n;
-    k.minima = minima;
-    k.maxima = maxima;
-    std::vector<std::vector<int>> part;
-
-    //std::cout << "Time taken for retrieving functions: " << total_time << "\n";
-    //std::cout << "Time taken for old functions: " << original_time << "\n";
-    //std::cout << "Time taken for hasing functions: " << hashing_time << "\n";
-    //std::cout << "Old function ran: " << i << "\n";
-
-
     // Only one value to set?
     if(n == 1) {
         if (minima[0] <= N and N <= maxima[0]){
             partitions.push_back({N});
         }
         return;
-    } else if (hashTable.count(k) !=0) {
-        //auto start = std::chrono::high_resolution_clock::now();
-        //std::cout << "\nUsing existing hashtable: " << i;
-        //std::cout << "\nSize of hashTable: " << hashTable.size();
-        //indicates our key is already in the table, let's fish it out!
-
-        std::vector<std::vector<int>> ret = hashTable[k];
-        for (auto i : ret){
-            partitions.push_back(i);
-        }
-
-        //auto end = std::chrono::high_resolution_clock::now();
-        //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        //total_time+=(duration.count());
-
-        
-        return;
-
-    } else {
-        auto startO = std::chrono::high_resolution_clock::now();
-        i+=1;
-        // Create stack
-        struct SnapShotStruct {
-            std::vector<int> p;
-        };
-        std::stack<SnapShotStruct> snapshotStack;
-        
-        // Add first snapshot
-        SnapShotStruct currentSnapshot;
-        currentSnapshot.p = {};
-        snapshotStack.push(currentSnapshot);
-        
-        // Run...
-        while(!snapshotStack.empty())
-        {
-            
-            // pick the top snapshot and delete it from the stack
-            currentSnapshot= snapshotStack.top();
-            snapshotStack.pop();
-            
-            // current sum
-            int current_sum = std::accumulate(currentSnapshot.p.begin(), currentSnapshot.p.end(), 0);
-
-            // position at which we add
-            int pos = currentSnapshot.p.size();            
-            
-            // there are at least two more values to be set
-            if (pos < n-1){
-            
-                // Compute min and max for this placement
-                int max = maxima[pos];
-                if (N - current_sum < max ){
-                    max = N - current_sum;
-                }
-                
-                // Iterate over these values
-                for (int i = minima[pos]; i <= max; i++){
-                    std::vector<int> new_partition = currentSnapshot.p;
-                    new_partition.push_back(i);
-                    SnapShotStruct newSnapshot;
-                    newSnapshot.p = new_partition;
-                    snapshotStack.push(newSnapshot);
-                }
-                
-            }
-            
-            // only one more value to be set
-            else if ((pos == n-1) && (minima[pos] <= N - current_sum) && (N - current_sum <= maxima[pos])){
-                
-                std::vector<int> new_partition = currentSnapshot.p;
-                new_partition.push_back(N - current_sum);
-                
-                partitions.push_back(new_partition);
-                part.push_back(new_partition);
-
-                //auto endO = std::chrono::high_resolution_clock::now();
-                //auto durationO = std::chrono::duration_cast<std::chrono::microseconds>(endO - startO);
-                //original_time+=(durationO.count());
-                
-
-                //add new vectors to hashtable
-                //auto start = std::chrono::high_resolution_clock::now();
-                
-                //std::vector<std::vector<int>> newV = partitions;
-                
-                hashTable[k] = part;
-                //auto end = std::chrono::high_resolution_clock::now();
-                //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-                //hashing_time+=(duration.count());
-
-
-
-            }
-            
-        }
     }
+    
+    // Create stack
+    struct SnapShotStruct {
+        std::vector<int> p;
+    };
+    std::stack<SnapShotStruct> snapshotStack;
+    
+    // Add first snapshot
+    SnapShotStruct currentSnapshot;
+    currentSnapshot.p = {};
+    snapshotStack.push(currentSnapshot);
+    
+    // Run...
+    while(!snapshotStack.empty())
+    {
+        
+        // pick the top snapshot and delete it from the stack
+        currentSnapshot= snapshotStack.top();
+        snapshotStack.pop();
+        
+        // current sum
+        int current_sum = std::accumulate(currentSnapshot.p.begin(), currentSnapshot.p.end(), 0);
 
+        // position at which we add
+        int pos = currentSnapshot.p.size();            
+        
+        // there are at least two more values to be set
+        if (pos < n-1){
+        
+            // Compute min and max for this placement
+            int max = maxima[pos];
+            if (N - current_sum < max ){
+                max = N - current_sum;
+            }
+            
+            // Iterate over these values
+            for (int i = minima[pos]; i <= max; i++){
+                std::vector<int> new_partition = currentSnapshot.p;
+                new_partition.push_back(i);
+                SnapShotStruct newSnapshot;
+                newSnapshot.p = new_partition;
+                snapshotStack.push(newSnapshot);
+            }
+            
+        }
+        
+        // only one more value to be set
+        else if ((pos == n-1) && (minima[pos] <= N - current_sum) && (N - current_sum <= maxima[pos])){
+            std::vector<int> new_partition = currentSnapshot.p;
+            new_partition.push_back(N - current_sum);
+            partitions.push_back(new_partition);
+        }
+        
+    }
     
 }
 
