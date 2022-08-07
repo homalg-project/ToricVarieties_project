671a54b4693531ac8d2938647421a83befedad41
Martin Bies <MBies87@googlemail.com>
2022-08-07 09:28:24 +0200

    Root counter handles first amputated diagram

--- QSMExplorer/bin/PartialBlowups/Auxilliary/tree_like_computations.cpp
+++ QSMExplorer/bin/PartialBlowups/Auxilliary/tree_like_computations.cpp
@@ -419,27 +419,6 @@ void h0_from_partial_blowups(const std::vector<int>& degrees,
     // (5) Iterate over the connected components
     for (int i = 0; i < connected_components.size(); i++){
         
-        // count the number of amputated components
-        int amputated_components = 0;
-        for (int j = 0; j < connected_components[i].size(); j++){
-            int count_for_component_j = 0;
-            for (int k = 0; k < edges_of_connected_components[i].size(); k++){
-                if (edges_of_connected_components[i][k][0] == connected_components[i][j]){
-                    count_for_component_j++;
-                }
-                if (edges_of_connected_components[i][k][1] == connected_components[i][j]){
-                    count_for_component_j++;
-                }
-                if (count_for_component_j > 1){
-                    break;
-                }
-            }
-            
-            if (count_for_component_j == 1){
-                amputated_components++;
-            }
-        }
-        
         // tree-like case
         if (edges_of_connected_components[i].size()+1-connected_components[i].size() == 0){
             std::vector<int> helper_degrees;
@@ -449,7 +428,140 @@ void h0_from_partial_blowups(const std::vector<int>& degrees,
             }
             h0 += h0_on_rational_tree(connected_components[i], helper_degrees, edges_of_connected_components[i], details);
         }
+        
+        // handle a couple of simple cases with b1 = 1
+        else if (edges_of_connected_components[i].size()+1-connected_components[i].size() == 1 
+            && connected_components[i].size() >= 2 && connected_components[i].size() <= 3){
+        
+            // count local sections
+            int local_sections = 0;
+            for (int j = 0; j < connected_components[i].size(); j++){
+                if (degree_correspondence[connected_components[i][j]] >= 0){
+                    local_sections += degree_correspondence[connected_components[i][j]] + 1;
+                }
+            }
+            
+            // estimate conditions
+            int conditions = 0;
+            for (int j = 0; j < edges_of_connected_components[i].size(); j++){
+                int d1 = degree_correspondence[edges_of_connected_components[i][j][0]];
+                int d2 = degree_correspondence[edges_of_connected_components[i][j][1]];
+                if ((d1 >= 0) || (d2 >= 0)){
+                    conditions++;
+                }
+            }
+            
+            // increase h0
+            if (local_sections >= conditions){
+                h0 += local_sections - conditions;
+            }
+            
+            // check if we merely deal with a lower bound
+            
+            if (connected_components[i].size() == 2){
+                
+                // simple circuit of two components
+                // -> easy to deal with
+                if (degree_correspondence[connected_components[i][0]] == 0
+                    && degree_correspondence[connected_components[i][1]] == 0){
+                        lower_bound = true;
+                }
+                
+            }
+            
+            if (connected_components[i].size() == 3){
+                
+                // Slightly more involved case, as 3 components can form b1 in different ways
+                // -> Need to tell those configurations apart.
+                
+                // find out if there is an amputated component
+                std::vector<int> copy_of_components(connected_components[i]);
+                std::vector<int> amputated_components;
+                std::vector<int> connected_to;
+                for (int j = 0; j < connected_components[i].size(); j++){
+                    int count_for_component_j = 0;
+                    for (int k = 0; k < edges_of_connected_components[i].size(); k++){
+                        if (edges_of_connected_components[i][k][0] == connected_components[i][j]){
+                            count_for_component_j++;
+                        }
+                        if (edges_of_connected_components[i][k][1] == connected_components[i][j]){
+                            count_for_component_j++;
+                        }
+                        if (count_for_component_j > 1){
+                            break;
+                        }
+                    }
 
+                    if (count_for_component_j == 1){
+                        amputated_components.push_back(connected_components[i][j]);
+                        //copy_of_components.erase(copy_of_components.begin()+j);
+                        for (int k = 0; k < edges_of_connected_components[i].size(); k++){
+                            int connector;
+                            if (edges_of_connected_components[i][k][0] == connected_components[i][j]){
+                                connector = edges_of_connected_components[i][k][1];
+                                connected_to.push_back(connector);
+                            }
+                            if (edges_of_connected_components[i][k][1] == connected_components[i][j]){
+                                connector = edges_of_connected_components[i][k][0];
+                                connected_to.push_back(connector);
+                            }
+                            int pos = std::distance(copy_of_components.begin(),std::find(copy_of_components.begin(), copy_of_components.end(), connector));
+                            //copy_of_components.erase(copy_of_components.begin()+pos);
+                        }
+                    }
+                }
+                
+                // Case 1: Circuit of 3 components
+                int d1,d2,d3;
+                if (amputated_components.size() == 0){
+                    
+                    d1 = degree_correspondence[connected_components[i][0]];
+                    d2 = degree_correspondence[connected_components[i][1]];
+                    d3 = degree_correspondence[connected_components[i][2]];
+                    
+                    if (d1 == 0 && d2 == 0 && d3 == 0){
+                        lower_bound = true;
+                    }
+                }
+                // Case 2: Circuit of 2 components and one amputated component
+                else if (amputated_components.size() == 1){
+                    
+                    // find out to what components the amputated component is not connected to
+                    // TODO This is now special for 3 compoennts, i.e. at most one amputated component, one connected and
+                    // TODO one component which the amputated component is not connected to
+                    std::vector<int> not_connected_to;
+                    for (int j = 0; j < connected_components[i].size(); j++){
+                        if ((connected_components[i][j] != amputated_components[0])
+                            && (connected_components[i][j] != connected_to[0])){
+                            not_connected_to.push_back(connected_components[i][j]);
+                            break;
+                        }
+                    }
+                    //std::vector<int> not_connected_to(copy_of_components);
+                    
+                    // Consistency check
+                    if (amputated_components.size() != 1 || connected_to.size() != 1 || not_connected_to.size() != 1){
+                        //throw std::invalid_argument("\nPartition of circuit into amputated, connected and not connected went wrong.\n");
+                        std::cout << "\nProblem:\n";
+                        print_vector("Amputated:", amputated_components);
+                        print_vector("Connected:", connected_to);
+                        print_vector("Not connected:", not_connected_to);
+                        std::cout << "\n\n";
+                    }
+                    
+                    d1 = degree_correspondence[not_connected_to[0]];
+                    d2 = degree_correspondence[connected_to[0]];
+                    d3 = degree_correspondence[amputated_components[0]];
+                    
+                    if (d1 == 0 && d2 == 1 && d3 < 0){
+                        lower_bound = true;
+                    }
+                }
+            }
+            
+            
+        }
+        /*
         // simple circuit of two components
         else if (edges_of_connected_components[i].size() == 2 && connected_components[i].size() == 2){
             
@@ -471,7 +583,7 @@ void h0_from_partial_blowups(const std::vector<int>& degrees,
                 lower_bound = true;
             }
         }
-
+        
         // simple circuit of three components
         else if (edges_of_connected_components[i].size() == 3 && connected_components[i].size() == 3 && amputated_components == 0){
             
@@ -500,7 +612,7 @@ void h0_from_partial_blowups(const std::vector<int>& degrees,
                 lower_bound = true;
             }
             
-        }
+        }*/
         
         // beyond what we can currently handle
         else{
